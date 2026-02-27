72d3a61e60a2f453cf694712a6b811fba53db156
wolfecj1_appstate <wolfecj1@appstate.edu>
2026-02-27 15:14:01 -0500

    Create lc3.asm

---
+++ SYS/LC3 Programs/lc3.asm
@@ -0,0 +1,25 @@
+Register    before  after
+PC        x3404     
+IR        xC000     
+R0        x0018     
+R1        x0008     
+R2        x000F     
+R3        x0003     
+R4        x001C     
+R5        x000C     
+R6        x0002     
+R7        x001A     
+CC        P         
+
+Address ValueB      ValueAfter
+x3403   x5F46                   AND R7, R5, R6.   00011010, 00001100, 1000
+x3404   x59EC                   AND R4, R7, #12     00011100, 00001000, 
+x3405   xC140                   JMP R5
+x3406   x0009       
+x3407   x0014       
+x3408   x000E       
+x3409   x001C       
+x340A   x0004       
+x340B   x000E       
+x340C   x000A       
+
