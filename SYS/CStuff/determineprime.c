742731e8c4fe3c839e0363227858ed52c4a53f17
Chris <65196986+cjwolfe@users.noreply.github.com>
2026-04-05 18:13:40 -0400

    prime

--- SYS/CStuff/determineprime.c
+++ SYS/CStuff/determineprime.c
@@ -1,39 +1,50 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+
+//todo: fix why the arrays only go up to 6/7
+// figure out how to determine prime
+// figure out how to get math cos(n) to work
+
+const int primes[] = 
+        {2, 3,	5, 7,	11, 13,	17,	19,	23,	29,	
+        31,	37,	41,	43,	47,	
+        53,	59,	61,	67,	71};
 
 
-int isprime(int n){
-    if(n == 1){
-        return 0;
-    } else if(n %2 == 0){
-        return 1;
-    } else {
-        int m = n-1;
-        return isprime(m);
-    }
-
-
+int factorial(int n){
+    if(n == 0) return 1;
+    else
+    return n * factorial(n-1);
 }
 
-int max(int i, int j){
+int factorit(int n){
+    return (factorial(n + 1));
+}
+
+int min(int i, int j){
     if(i < j){
-        return j;
-    } else return i;
+        return i;
+    } else return j;
 }
 
 
-void printArrays(int *arr1, int *arr2){
+
+
+void printArray(int *arr1, int len1){
     printf("Count\tmine\tPrimes\n");
 
-    int length = max(sizeof(arr1), sizeof(arr2));
+    int primesLen = sizeof(primes) / sizeof(primes[0]);
+
+    int length = (len1 < primesLen) ? len1 : primesLen;
 
     // length = sizeof(arr1);
     // length = sizeof(arr2);
     
     for(int i = 0; i < length; i++){
         
-        printf("%i\t%i\t%i\n",i,arr1[i],arr2[i]);
-        printf("length == %i\n",length);
+        printf("%i\t%i\t%i\n",i + 1,arr1[i],primes[i]);
+        // printf("length == %i\n",length);
 
     }
 }
@@ -43,23 +54,27 @@ void printArrays(int *arr1, int *arr2){
 int main(){
 
     int array[20];
+    int numElements = sizeof(array)/sizeof(array[0]);
+    for(int i = 0; i < numElements; i++){
+        array[i] = i;
+    }
     // memset(array, 1, sizeof(array));
 
 
-    int primes[20] = 
-        {2, 3,	5, 7,	11,
-        13,	17,	19,	23,	29,	
-        31,	37,	41,	43,	47,	
-        53,	59,	61,	67,	71};
 
 
-    // for(int i = 0; i < sizeof(array)-1; i++){
-    //     array[i] = i;
+
+
+    for(int i = 0; i < numElements; i++){
+        // array[i] = factorial(array[i]);
+        array[i] = array[i] * array[i];
+    }
+
+
+    // for(int i = 0; i < 20; i++){
+
     // }
-
-
-
-    printArrays(array,primes);
+    printArray(array, numElements);
 
     // printf("length = %i",length);
 
