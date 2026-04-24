8120f6296abd7a5c3225262f1396e1c52ccf2dfd
Chris <65196986+cjwolfe@users.noreply.github.com>
2026-04-23 22:55:09 -0400

    LAb 7

--- DSA/Lab7/student/Jugs.java
+++ DSA/Lab7/student/Jugs.java
@@ -1,35 +1,88 @@
 package student;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Jugs
 {
 	private static int A, B, C;
 	private static boolean[][] visited;
 	private static final int MAX_VALUE = 1000 + 1;
+	// private static StringBuilder log = new StringBuilder();
+	private static Stack log = new Stack<Character>();
 
 	public static String solve(int a, int b, int c)
 	{
 		A = a; B = b; C = c;
 		visited = new boolean[MAX_VALUE][MAX_VALUE];
-		// TODO: call a private method here to
-		// generate a solution string and return it
+		log.clear();
+
+		if(dfs(0,0)){
+			// at the goal, time to send out
+			StringBuilder sb = new StringBuilder();
+			// while(!log.isEmpty()){sb.append(log.pop());}
+			Stack rev = new Stack<Character>();
+			while(!log.isEmpty()){rev.push(log.pop());}
+			while(!rev.isEmpty()){sb.append(rev.pop());}
+			
+			return sb.toString();
+		}
+
 		return null;
 	}
+
+
+	private static boolean dfs(int a, int b){
+		if(a + b == C) return true; // goooooooooooal
+		if(visited[a][b]) return false;
+
+		visited[a][b] = true;
+
+		if(tryMove(A,b,'A')) return true;
+		if(tryMove(a,B,'B')) return true;
+		if(tryMove(0,b,'C')) return true;
+		if(tryMove(a,0,'D')) return true;
+
+		int pourToTwo = Math.min(a, B - b);
+		if(tryMove(a - pourToTwo, b + pourToTwo, 'E')) return true;
+
+		int pourToOne = Math.min(b, A - a);
+		if(tryMove(a + pourToOne, b - pourToOne, 'F')) return true;
+
+		return false;
+	}
+
+private static boolean tryMove(int nextA, int nextB, char move) {
+        if (nextA >= 0 && nextA <= A && nextB >= 0 && nextB <= B && !visited[nextA][nextB]) {
+            log.push(move); // store 
+            if (dfs(nextA, nextB)) {
+                return true;
+            }
+            log.pop(); // backtrack
+        }
+        return false;
+    }
+
+
 
 	public static void main(String[] args)
 	{
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter A: ");
+		// A = 3;
 		A = scan.nextInt();
 		System.out.print("Enter B: ");
+		// B = 4;
 		B = scan.nextInt();
 		System.out.print("Enter C: ");
+		// C = 5;
 		C = scan.nextInt();
 
 		String steps = solve(A, B, C);
 		if (steps == null)
 			System.out.println("Impossible!");
 		else System.out.println("Possible: " + steps);
+		// System.out.println("\n" + log.toString());
+		scan.close();
 	}
 }
--- DSA/Lab7/student/WordLadder.java
+++ DSA/Lab7/student/WordLadder.java
@@ -5,6 +5,8 @@ import java.io.*;
 
 public class WordLadder
 {
+	private static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+	private static HashSet dictionary = new HashSet();
 	public static List<String> solve(String dictfile, String start, String end)
 	{
 		try
@@ -12,17 +14,81 @@ public class WordLadder
 			File dict = new File(dictfile);
 			Scanner scan = new Scanner(dict);
 			while (scan.hasNext())
-				; // add scan.next() to the collection of words
+				dictionary.add(scan.next());
 			scan.close();
+
 		}
 		catch (IOException e)
 		{
 			return null;
 		}
 
-		// TODO: create a word ladder
+		if(!dictionary.contains(start) || !dictionary.contains(end)){
+			return null;
+		}
+
+		// System.out.println("The difference between " + start + " and " + end + " is " + difference(start,end));
+
+		Queue<String> queue = new LinkedList<>();
+		Map<String, String> parentMap = new HashMap<>();
+
+		queue.add(start);
+		parentMap.put(start,null);
+
+		while(!queue.isEmpty()){
+			String current = queue.poll();
+			
+			if(current.equals(end)){
+				LinkedList<String> path = new LinkedList<>();
+				String step = end;
+				
+				while (step != null) {
+					path.addFirst(step);
+					step = parentMap.get(step);
+				}
+				
+				return path;
+			}
+			bfs(current,queue,parentMap);
+
+		}
 		return null;
 	}
+
+	public static void bfs(String current, Queue<String> queue, Map<String, String> parentMap) {
+		char[] chars = current.toCharArray();
+
+		for (int i = 0; i < chars.length; i++) {
+			char originalChar = chars[i];
+
+			for (char c : alphabet) {
+				if (c == originalChar) continue;
+
+				chars[i] = c;
+				String neighbor = new String(chars);
+
+				if (dictionary.contains(neighbor) && !parentMap.containsKey(neighbor)) {
+					parentMap.put(neighbor, current);
+					queue.add(neighbor);
+				}
+			}
+			chars[i] = originalChar;
+		}
+	}
+
+	// public static int difference(String m, String n){
+	// 	int count = 0;
+	// 	// char [] M = m.toCharArray();
+	// 	// char [] N = n.toCharArray();
+
+	// 	for(int i = 0; i < m.length(); i++){
+	// 		if(m.charAt(i) != n.charAt(i)){
+	// 			count++;
+	// 		}
+	
+	// 	}
+	// 	return count;
+	// }
 
 	// call this with 1 command line argument
 	// args[0] should be the relative path to "dictionary4.txt"
@@ -34,6 +100,9 @@ public class WordLadder
 		System.out.print("Enter the end word: ");
 		String end = scan.next();
 
+		String testing = "dictionary4.txt";
+
+		// replace testing with args[0] for submit
 		List<String> solution = solve(args[0], start, end);
 		if (solution == null)
 			System.out.println("Impossible!");
@@ -43,5 +112,6 @@ public class WordLadder
 			for (String word : solution)
 				System.out.println(word);
 		}
+		scan.close();
 	}
 }
