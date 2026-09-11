795e911997c608b7b74f807ba3730c474155b047
cjwolfe <65196986+cjwolfe@users.noreply.github.com>
2026-09-11 00:52:27 -0400

    Hw 3 and 4 systems upload

--- SYS/Assignments/hwSub3Mem.asm
+++ SYS/Assignments/hwSub3Mem.asm
@@ -1,6 +1,21 @@
 .orig 3000 ;begin at 3000
 LD R0, #6	;
+ld r1, #6;
+not r1, r1;
+add r1, r1, #1;
+add r0,r0, r1;
+ld r1, #6;
+not r1,r1;
+add r1, r1, #1;
+add r0, r0, r1;
+ld r1, #6;
+not r1, r1;
+add r1, r1, #1;
+add r0, r0, r1;
+; print to output
 HALT ; f025
+
+
 10 ; start with this value
 4 ; subtract this value
 2 ; subtract this value
